2ccf73b3ac7069127242a83226e6c7be4ac2a52f
Jin <passawit53@gmail.com>
2025-09-09 23:24:48 +0700

    Exclude midori-infastructure from Git tracking and TypeScript compilation

--- midori-infastructure/lambda/api/prisma/migrations/20250829094902_update_type/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `prompt` column on the `Generation` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Generation" DROP COLUMN "prompt",
-ADD COLUMN     "prompt" JSONB;
